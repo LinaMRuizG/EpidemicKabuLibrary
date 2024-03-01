a309e3666e6fb2a4bb637de05de0993b0aa34b5a
linar <linar@wolfram.com>
2024-03-01 14:44:04 -0500

    delete workflow

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-    description = fh.read()
+    long_description = fh.read()
 
 # Setting up
 setup(
     name="epidemickabu",
-    version='1.0.4',
+    version='0.1.0',
     description="New methodology to identify waves, peaks, and valleys from epidemic curve",
-    long_description=description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LinaMRuizG/EpidemicKabuLibrary",
     author="Lina M. Ruiz G. and Anderson A. Ruales B.",
-    author_email="<lina.ruiz2@udea.edu.co>",
+    author_email="lina.ruiz2@udea.edu.co",
     license = 'MIT',
     packages=find_packages(include=['epidemickabu']),
     install_requires=['pandas==1.5.3', 'scipy==1.10.1', 'matplotlib==3.7.1','numpy==1.25.2'],
